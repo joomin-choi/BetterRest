26e649b6665baab91cfa3fb605fd4c7378e92313
JooMin Choi <joomin.choi@clearscore.com>
2022-09-12 14:28:03 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by JooMin Choi on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
